01e35292662178d7fb2cdd31916868dd9b859fb2
neha-patankar <nehapatankar122@gmail.com>
2026-02-10 17:29:02 +0530

    Add client, server, docs folders

---
+++ client/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import JobSystemHistory from "./components/JobSystemHistory"
+
+export default function Home() {
+  return (
+    <div>
+    
+     <JobSystemHistory/>
+    </div>
+  );
+}
